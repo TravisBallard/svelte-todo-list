b4ddb9752f27a58df66c6f5a3e57e62fba464a2a
Travis Ballard <travis.ballard@gmail.com>
2020-10-22 08:00:43 -0700

    initial commit - made some changes after scaffolding

---
+++ src/routes/addItem.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let items = []
+    const addItem = (item) => items.push(item)
+
+    console.log('items', items)
+</script>
+
+<style>
+    h2 {
+        font-size: 1.4em;
+    }
+</style>
+
+<h2>Add item</h2>
+<form>
+    <div>
+        <label for="item">Item to add:</label>
+        <input type="text" id="item" value="" placeholder="Enter item here." size="100">
+    </div>
+    <div>
+        <input type="submit" value="Submit">
+    </div>
+</form>
---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script>
+	export let items = [
+		{
+			id: 1,
+			title: 'First TODO item.',
+			completed: false,
+		}
+	]
+	const itemsLength = items.length
+</script>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>My ToDo List</title>
+</svelte:head>
+
+<h1>{itemsLength} Items in TODO list</h1>
+<ul>
+	{#each items as todo, index (todo.id)}
+		<li>
+			<input type="checkbox" checked={todo.completed}/> {todo.title}
+		</li>
+	{:else}
+		<li>Nothing to do here!</li>
+	{/each}
+</ul>
